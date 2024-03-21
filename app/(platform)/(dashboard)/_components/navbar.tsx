bdd812312f874e681bc39e6eac420f8c56399cba
unknown <waelmanai96@gmail.com>
2024-03-21 01:18:05 +0100

    Dashboard Sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,20 +4,22 @@ import { Plus } from "lucide-react"
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
 
+import { MobileSidebar } from "./mobile-sidebar"
+
 
 export const Navbar = () => {
     return (
         <header>
             <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-                {/* Mobile Menu */}
+                <MobileSidebar />
                 <div className="flex items-center gap-x-4">
                     <div className="hidden md:flex">
                         <Logo />
                     </div>
-                    <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                    <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
                         Create
                     </Button>
-                    <Button size="sm" className="rounded-sm block md:hidden">
+                    <Button variant="primary" size="sm" className="rounded-sm block md:hidden">
                         <Plus className="size-4" />
                     </Button>
                 </div>
