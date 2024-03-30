03af5a95fed926fdcc03493b7901117c6a823081
unknown <waelmanai96@gmail.com>
2024-03-30 16:24:52 +0100

    Query Provider

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 
 const PlatformLayout = ({
@@ -11,9 +12,11 @@ const PlatformLayout = ({
 }) => {
     return (
         <ClerkProvider>
-            <Toaster />
-            <ModalProvider />
-            {children}
+            <QueryProvider>
+                <Toaster />
+                <ModalProvider />
+                {children}
+            </QueryProvider>
         </ClerkProvider>
     )
 }
