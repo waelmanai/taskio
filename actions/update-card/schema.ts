83861971db627e1e5a09bfe19b721626c282ba60
unknown <waelmanai96@gmail.com>
2024-03-31 02:07:30 +0100

    Card Header

---
+++ actions/update-card/schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const UpdateCard = z.object({
+    title: z.string({
+        required_error: "Title is required",
+        invalid_type_error: "Title is required",
+    }).min(3, {
+        message: "Title is too short."
+    }),
+    description: z.optional(
+        z.string({
+            required_error: "Description is required",
+            invalid_type_error: "Description is required",
+        }).min(3, {
+            message: "Description is too short."
+        })
+    ),
+    id: z.string(),
+    boardId: z.string()
+});
+
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+
 import { useCardModal } from "@/hooks/use-card-modal";
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
+
+import { Header } from "./header";
+
 
 export const CardModal = () => {
 
@@ -9,13 +17,21 @@ export const CardModal = () => {
     const isOpen = useCardModal((state) => state.isOpen);
     const onClose = useCardModal((state) => state.onClose);
 
+    const { data: cardData } = useQuery<CardWithList>({
+        queryKey: ["cards", id],
+        queryFn: () => fetcher(`/api/cards/${id}`),
+    });
+
     return (
         <Dialog
             open={isOpen}
             onOpenChange={onClose}
         >
             <DialogContent>
-                Modal
+                {!cardData
+                    ? <Header.Skeleton />
+                    : <Header data={cardData} />
+                }
             </DialogContent>
         </Dialog>
     )
